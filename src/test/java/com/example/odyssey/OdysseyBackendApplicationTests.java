396b48bea426bd56bc9f016fa994413589f7a9f9
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-11-21 11:59:20 +0100

    Add basic project

---
+++ src/test/java/com/example/odyssey/OdysseyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.odyssey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdysseyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
