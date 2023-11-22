f840ad493e71263ceb57088b4db0af45c689f3af
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-11-22 14:18:47 +0100

    Add TimeSlot entity

--- src/main/java/com/example/odyssey/entity/TimeSlot.java
+++ src/main/java/com/example/odyssey/entity/TimeSlot.java
@@ -1,7 +1,24 @@
 package com.example.odyssey.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TimeSlot {
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+    public Duration getDuration() {
+        return Duration.between(start, end);
+    }
 }
