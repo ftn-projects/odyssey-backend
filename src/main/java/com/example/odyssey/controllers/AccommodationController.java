57a12e4baf8c23d96bce4c7ea01f6f49ded028cf
Arezinko <arezinamilann@gmail.com>
2023-11-23 00:04:40 +0100

    Add guidelines for accommodation, notification and report controllers

--- src/main/java/com/example/odyssey/controllers/AccommodationController.java
+++ src/main/java/com/example/odyssey/controllers/AccommodationController.java
@@ -6,4 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/v1/accommodations")
 public class AccommodationController {
+
+        // GET AllAccommodations()
+        // GET Accommodation(Long accommodationID)
+        // GET AccommodationByHost(Long userID)
+        // POST CreateAccommodation(RequestAccommodationDTO accommodation)
+        // PUT UpdateAccommodation(RequestAccommodationDTO accommodation)
+        // DELETE DeleteAccommodation(Long accommodationID)
 }
--- src/main/java/com/example/odyssey/controllers/NotificationController.java
+++ src/main/java/com/example/odyssey/controllers/NotificationController.java
@@ -6,4 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/v1/notifications")
 public class NotificationController {
+
+
+    // GET AllUserNotifications(Long userID)
+    // GET UserNotifications(Long userID, String type)
+    // GET AllUnreadUserNotifications(Long userID)
+    // PUT OpenNotification(Long notificationID)
+    // PUT OpenAllNotifications(Long userID)
+
+
+
 }
