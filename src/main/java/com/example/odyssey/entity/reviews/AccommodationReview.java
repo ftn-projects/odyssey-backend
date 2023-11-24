39e82235a75950db88ea84e53f45db0a2ef0a687
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-11-24 17:26:53 +0100

    Model update

--- src/main/java/com/example/odyssey/entity/reviews/AccommodationReview.java
+++ src/main/java/com/example/odyssey/entity/reviews/AccommodationReview.java
@@ -1,12 +1,16 @@
 package com.example.odyssey.entity.reviews;
 
 import com.example.odyssey.entity.accommodations.Accommodation;
+import com.example.odyssey.entity.users.Guest;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -15,5 +19,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @Table(name = "accommodation_reviews")
 public class AccommodationReview extends Review {
+    @ManyToOne
     private Accommodation accommodation;
+
+    public AccommodationReview(Long id, Double rating, String comment, Status status, LocalDateTime submissionDate, Guest submitter, Accommodation accommodation) {
+        super(id, rating, comment, status, submissionDate, submitter);
+        this.accommodation = accommodation;
+    }
 }
--- src/main/java/com/example/odyssey/entity/users/Guest.java
+++ src/main/java/com/example/odyssey/entity/users/Guest.java
@@ -1,5 +1,6 @@
 package com.example.odyssey.entity.users;
 
+import com.example.odyssey.entity.Address;
 import com.example.odyssey.entity.accommodations.Accommodation;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -7,16 +8,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Map;
 import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @DiscriminatorValue(value = "GUEST")
 public class Guest extends User {
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "guest_favourited", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "accommodation_id", referencedColumnName = "id"))
     private Set<Accommodation> favourites;
+
+    public Guest(Long id, Role role, AccountStatus status, String name, String surname, String email, String password, Address address, String phone, String profileImage, Map<String, String> settings, Set<Accommodation> favourites) {
+        super(id, role, status, name, surname, email, password, address, phone, profileImage, settings);
+        this.favourites = favourites;
+    }
 }
