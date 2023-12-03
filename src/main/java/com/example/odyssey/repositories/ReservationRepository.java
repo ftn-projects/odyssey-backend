d196ac8437f35d07ce71840ef1c4bb81d11cadfd
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-12-03 13:43:43 +0100

    Implement service into reservation controller

--- src/main/java/com/example/odyssey/repositories/ReservationRepository.java
+++ src/main/java/com/example/odyssey/repositories/ReservationRepository.java
@@ -6,10 +6,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     @NonNull Page<Reservation> findAll(@NonNull Pageable pageable);
-    Reservation findReservationByAccommodation_Id(Long id);
-    Reservation findReservationByGuest_Id(Long id);
-    Reservation findReservationByAccommodation_Host_Id(Long id);
 
+    Reservation findReservationsById(Long id);
+    List<Reservation> findReservationsByAccommodation_Id(Long id);
+    List<Reservation> findReservationsByGuest_Id(Long id);
+    List<Reservation> findReservationsByAccommodation_Host_Id(Long id);
+    List<Reservation> findReservationsByStatus(Reservation.Status status);
+    List<Reservation> findReservationsByTimeSlot_Start(LocalDateTime start);
+    List<Reservation> findReservationsByTimeSlot_End(LocalDateTime end);
 }
