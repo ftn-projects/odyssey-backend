e9c431d592d247e9403a504d30db35015db68584
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-12-03 11:48:42 +0100

    Minor code cleanup

--- src/main/java/com/example/odyssey/services/ReservationService.java
+++ src/main/java/com/example/odyssey/services/ReservationService.java
@@ -3,7 +3,6 @@ package com.example.odyssey.services;
 import com.example.odyssey.entity.reservations.Reservation;
 import com.example.odyssey.repositories.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
