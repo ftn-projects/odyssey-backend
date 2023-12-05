fcc41afa7f58b2ba6bf69fea89ea001fe34b3ca6
Arezinko <arezinamilann@gmail.com>
2023-12-05 13:00:21 +0100

    Add logic for accommodation filtering

--- src/main/java/com/example/odyssey/repositories/AccommodationRepository.java
+++ src/main/java/com/example/odyssey/repositories/AccommodationRepository.java
@@ -7,15 +7,46 @@ import com.example.odyssey.entity.users.Host;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
 
     @NonNull
-    Page<Accommodation> findAll(Pageable page);
+    @Query("SELECT DISTINCT a " +
+            "FROM Accommodation a " +
+            "JOIN FETCH a.amenities am " +
+            "WHERE (:startGuests IS NULL OR a.minGuests BETWEEN :startGuests AND :endGuests) " +
+            "  AND (:endGuests IS NULL OR a.maxGuests BETWEEN :startGuests AND :endGuests) " +
+            "  AND (:type IS NULL OR a.type = :type) " +
+            "  AND (COALESCE(:amenityIds, NULL) IS NULL OR am.id IN :amenityIds) " +
+            "  AND NOT EXISTS (" +
+            "    SELECT 1 " +
+            "    FROM Reservation r " +
+            "    WHERE r.accommodation = a " +
+            "      AND (:reservationStartDate IS NULL OR r.endDate > :reservationStartDate) " +
+            "      AND (:reservationEndDate IS NULL OR r.startDate < :reservationEndDate) " +
+            "  ) " +
+            "  AND EXISTS (" +
+            "    SELECT 1 " +
+            "    FROM a.availableSlots s " +
+            "    WHERE s.timeSlot.start <= :reservationEndDate " +
+            "      AND s.timeSlot.end >= :reservationStartDate " +
+            "      AND (:startSlotPrice IS NULL OR s.price BETWEEN :startSlotPrice AND :endSlotPrice)")
+    List<Accommodation> findAccommodationsWithFilter(
+            @Param("startGuests") Integer startGuests,
+            @Param("endGuests") Integer endGuests,
+            @Param("type") Accommodation.Type type,
+            @Param("amenityIds") List<Long> amenityIds,
+            @Param("reservationStartDate") LocalDate reservationStartDate,
+            @Param("reservationEndDate") LocalDate reservationEndDate,
+            @Param("startSlotPrice") Double startSlotPrice,
+            @Param("endSlotPrice") Double endSlotPrice);
 
     Accommodation findOneById(Long id);
 
