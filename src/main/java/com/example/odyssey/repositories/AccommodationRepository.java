0b0356447487a9335a302ec663ca3c18463ff911
Arezinko <arezinamilann@gmail.com>
2023-12-06 13:28:49 +0100

    Fix logic for filtering not working properly

--- src/main/java/com/example/odyssey/repositories/AccommodationRepository.java
+++ src/main/java/com/example/odyssey/repositories/AccommodationRepository.java
@@ -29,14 +29,14 @@ public interface AccommodationRepository extends JpaRepository<Accommodation, Lo
             "    SELECT 1 " +
             "    FROM Reservation r " +
             "    WHERE r.accommodation = a " +
-            "      AND (cast(:reservationStartDate as localdatetime) IS NOT NULL AND r.timeSlot.end > :reservationStartDate) " +
-            "      AND (cast(:reservationEndDate as localdatetime) IS NOT NULL AND r.timeSlot.start < :reservationEndDate)) " +
+            "      AND (cast(:reservationStartDate as localdatetime) IS NOT NULL AND cast(:reservationEndDate as localdatetime) IS NOT NULL) " +
+            "      AND (r.timeSlot.end > :reservationStartDate AND r.timeSlot.start < :reservationEndDate)) " +
             "  AND EXISTS (" +
             "    SELECT 1 " +
             "    FROM a.availableSlots s " +
             "    WHERE (" +
-            "      (cast(:reservationStartDate as localdatetime) IS NULL OR s.timeSlot.end >= :reservationStartDate) " +
-            "      AND (cast(:reservationEndDate as localdatetime) IS NULL OR s.timeSlot.start <= :reservationEndDate)" +
+            "      (cast(:reservationStartDate as localdatetime) IS NOT NULL AND cast(:reservationEndDate as localdatetime) IS NOT NULL) " +
+            "      AND (s.timeSlot.end >= :reservationStartDate OR s.timeSlot.start <= :reservationEndDate)" +
             "    )" +
             "    AND ((cast(:startSlotPrice as double ) IS NULL OR cast(:endSlotPrice as double) IS NULL ) OR s.price BETWEEN :startSlotPrice AND :endSlotPrice))")
     List<Accommodation> findAllWithFilter(
