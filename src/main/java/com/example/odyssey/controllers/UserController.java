a5358c371f5b78b64e96ed9cdab00b93de8e04b6
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-12-07 01:18:15 +0100

    Fix broken imports

--- src/main/java/com/example/odyssey/controllers/UserController.java
+++ src/main/java/com/example/odyssey/controllers/UserController.java
@@ -1,16 +1,9 @@
 package com.example.odyssey.controllers;
 
-import com.example.odyssey.dtos.notifications.NotificationDTO;
 import com.example.odyssey.dtos.users.PasswordDTO;
 import com.example.odyssey.dtos.users.RegistrationDTO;
 import com.example.odyssey.dtos.users.UserDTO;
-import com.example.odyssey.entity.Address;
-import com.example.odyssey.entity.notifications.Notification;
-import com.example.odyssey.entity.reviews.HostReview;
-import com.example.odyssey.entity.users.Guest;
-import com.example.odyssey.entity.users.Host;
 import com.example.odyssey.entity.users.User;
-import com.example.odyssey.exceptions.IncorrectPasswordException;
 import com.example.odyssey.mappers.UserDTOMapper;
 import com.example.odyssey.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/users")
@@ -34,26 +27,27 @@ public class UserController {
     private final List<User> data = DummyData.getUsers();
 
     @GetMapping
-    public ResponseEntity<?> getAll(){
+    public ResponseEntity<?> getAll() {
         List<User> users = service.getAll();
 
         if (users.isEmpty()) return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        return new ResponseEntity<>(mapToDTO(users),HttpStatus.OK);
+        return new ResponseEntity<>(mapToDTO(users), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) {
         User user = service.find(id);
-        if(user == null) return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        if (user == null) return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(UserDTOMapper.fromUserToDTO(user), HttpStatus.OK);
     }
-// post
+
+    // post
     @GetMapping("/login/{email}/{password}")
     public ResponseEntity<?> login(@PathVariable String email, @PathVariable String password) {
         User user = data.get(1);
 
 //        user = service.login(email, password);
-        if(user == null) return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        if (user == null) return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(UserDTOMapper.fromUserToDTO(user), HttpStatus.OK);
     }
 
@@ -62,7 +56,7 @@ public class UserController {
         User user = UserDTOMapper.fromDTOtoUser(userDTO);
 
         user = service.update(user);
-        if (user == null) return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+        if (user == null) return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(UserDTOMapper.fromUserToDTO(user), HttpStatus.OK);
     }
 
