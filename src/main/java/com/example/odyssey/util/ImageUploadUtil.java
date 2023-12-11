aa5355df382174ec1a7de68296f2c03a94972111
Arezinko <arezinamilann@gmail.com>
2023-12-11 14:24:54 +0100

    Add ImageUploadUtils class (I forgot to git add it the last time)

--- src/main/java/com/example/odyssey/util/ImageUploadUtil.java
+++ src/main/java/com/example/odyssey/util/ImageUploadUtil.java
@@ -1,2 +1,32 @@
-package com.example.odyssey.util;public class ImageUploadUtil {
+package com.example.odyssey.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ImageUploadUtil {
+    public static void saveImage(String uploadDir, String fileName,
+                                 MultipartFile multipartFile) throws IOException {
+
+        Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+
+            System.out.println("FilePath is:" + uploadPath.toAbsolutePath());
+
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+    }
 }
