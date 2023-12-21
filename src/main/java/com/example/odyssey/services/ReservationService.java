4a630c7ead08a9d2f64e764dc5b0d1d66d6a7870
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-12-21 02:02:01 +0100

    Fix reservation bugs

--- src/main/java/com/example/odyssey/services/ReservationService.java
+++ src/main/java/com/example/odyssey/services/ReservationService.java
@@ -3,6 +3,7 @@ package com.example.odyssey.services;
 import com.example.odyssey.entity.TimeSlot;
 import com.example.odyssey.entity.reservations.Reservation;
 import com.example.odyssey.repositories.ReservationRepository;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,27 +18,53 @@ public class ReservationService {
     @Autowired
     private ReservationRepository reservationRepository;
 
-    public List<Reservation> getAll () {return reservationRepository.findAll();}
-    public Reservation find(Long id) {return reservationRepository.findReservationsById(id);}
-    public List<Reservation> findByAccommodation(Long id){return reservationRepository.findReservationsByAccommodation_Id(id);}
-    public List<Reservation> findByGuest(Long id){return reservationRepository.findReservationsByGuest_Id(id);}
-    public List<Reservation> findByHost(Long id){return reservationRepository.findReservationsByAccommodation_Host_Id(id);}
-    public Reservation save(Reservation reservation){return reservationRepository.save(reservation);}
+    public List<Reservation> getAll() {
+        return reservationRepository.findAll();
+    }
 
-    public List<Reservation> filter(List<Reservation> reservations, Long accommodationId, Reservation.Status status, LocalDateTime start, LocalDateTime end){
-        if (accommodationId!=null) reservations.retainAll(reservationRepository.findReservationsByAccommodation_Id(accommodationId));
-        if (status!=null) reservations.retainAll(reservationRepository.findReservationsByStatus(status));
-        if (start!=null) reservations.retainAll(reservationRepository.findReservationsByTimeSlot_Start(start));
-        if (end!=null) reservations.retainAll(reservationRepository.findReservationsByTimeSlot_End(end));
+    public Reservation find(Long id) {
+        return reservationRepository.findReservationsById(id);
+    }
+
+    public List<Reservation> findByAccommodation(Long id) {
+        return reservationRepository.findReservationsByAccommodation_Id(id);
+    }
+
+    public List<Reservation> findByGuest(Long id) {
+        return reservationRepository.findReservationsByGuest_Id(id);
+    }
+
+    public List<Reservation> findByHost(Long id) {
+        return reservationRepository.findReservationsByAccommodation_Host_Id(id);
+    }
+
+    public Reservation save(Reservation reservation) {
+        if (reservation.getGuestNumber() < reservation.getAccommodation().getMinGuests() ||
+                reservation.getGuestNumber() > reservation.getAccommodation().getMaxGuests())
+            throw new ValidationException("Reservation cannot be made.");
+
+        if (overlapsReservation(reservation.getAccommodation().getId(), reservation.getTimeSlot()))
+            throw new ValidationException("Reservation cannot be made.");
+        return reservationRepository.save(reservation);
+    }
+
+    public List<Reservation> filter(List<Reservation> reservations, Long accommodationId, Reservation.Status status, LocalDateTime start, LocalDateTime end) {
+        if (accommodationId != null)
+            reservations.retainAll(reservationRepository.findReservationsByAccommodation_Id(accommodationId));
+        if (status != null) reservations.retainAll(reservationRepository.findReservationsByStatus(status));
+        if (start != null) reservations.retainAll(reservationRepository.findReservationsByTimeSlot_Start(start));
+        if (end != null) reservations.retainAll(reservationRepository.findReservationsByTimeSlot_End(end));
 
         return reservations;
     }
-    public LocalDateTime convertToDate(Long date){
+
+    public LocalDateTime convertToDate(Long date) {
         return LocalDateTime.ofInstant(Instant.ofEpochMilli(date), TimeZone.getDefault().toZoneId());
     }
-    public boolean overlapsReservation(Long accommodationId, TimeSlot slot){
+
+    public boolean overlapsReservation(Long accommodationId, TimeSlot slot) {
         List<Reservation> reservations = findByAccommodation(accommodationId);
-        for(Reservation i:reservations)
+        for (Reservation i : reservations)
             if (i.getTimeSlot().overlaps(slot))
                 return true;
         return false;
