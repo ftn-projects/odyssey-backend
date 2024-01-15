6683c89a6c57104afbee4c4eea712f10547ccc41
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2024-01-16 00:21:07 +0100

    Update user controller and add UserWithReports DTO

--- src/main/java/com/example/odyssey/dtos/users/UserWithReportsDTO.java
+++ src/main/java/com/example/odyssey/dtos/users/UserWithReportsDTO.java
@@ -2,8 +2,9 @@ package com.example.odyssey.dtos.users;
 
 import com.example.odyssey.dtos.reports.UserReportDTO;
 import com.example.odyssey.entity.reports.UserReport;
+import com.example.odyssey.entity.users.Host;
+import com.example.odyssey.entity.users.Role;
 import com.example.odyssey.entity.users.User;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,12 +14,17 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class UserWithReportsDTO extends UserDTO {
+    private User.AccountStatus status;
+    private String role;
     private List<UserReportDTO> reports;
 
     public UserWithReportsDTO(User user, List<UserReport> reports) {
         super(user);
+        this.status = user.getStatus();
+        this.role = user.getRoles().get(0).getName();
         this.reports = reports.stream().map(UserReportDTO::new).toList();
+        if (user instanceof Host)
+            this.setBio(((Host) user).getBio());
     }
 }
--- src/main/java/com/example/odyssey/repositories/UserRepository.java
+++ src/main/java/com/example/odyssey/repositories/UserRepository.java
@@ -1,16 +1,35 @@
 package com.example.odyssey.repositories;
 
+import com.example.odyssey.entity.reviews.Review;
 import com.example.odyssey.entity.users.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @NonNull Page<User> findAll(@NonNull Pageable pageable);
+    @NonNull
+    Page<User> findAll(@NonNull Pageable pageable);
+
     Optional<User> findUserByEmail(String email);
+
     List<User> findUsersByStatus(User.AccountStatus status);
+
+    @Query("SELECT u " +
+            "FROM User u " +
+            "WHERE (:search IS NULL OR UPPER(CONCAT(u.name, ' ', u.surname)) LIKE %:search%) " +
+            "  AND (COALESCE(:roles, NULL) IS NULL OR EXISTS (SELECT 1 FROM u.roles r WHERE r.name IN :roles)) " +
+            "  AND (COALESCE(:statuses, NULL) IS NULL OR u.status IN :statuses) " +
+            "  AND (:reported IS NULL OR :reported = FALSE OR EXISTS (SELECT 1 FROM UserReport r WHERE r.reported.id = u.id))")
+    List<User> findAllByFilters(
+            @Param("search") String search,
+            @Param("roles") List<String> roles,
+            @Param("statuses") List<User.AccountStatus> statuses,
+            @Param("reported") Boolean reported);
 }
